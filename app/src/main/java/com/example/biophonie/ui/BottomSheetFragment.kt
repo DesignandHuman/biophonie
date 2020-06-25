81c8ba5cc698680aba5e7e72982e473b3be2cfb8
Haran <ara.rahier@gmail.com>
2020-06-25 15:15:31 +0200

    :art: Use databinding

--- app/src/main/java/com/example/biophonie/ui/BottomSheetFragment.kt
+++ app/src/main/java/com/example/biophonie/ui/BottomSheetFragment.kt
@@ -8,14 +8,12 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.view.*
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.biophonie.R
+import com.example.biophonie.databinding.BottomSheetLayoutBinding
 import com.example.biophonie.network.ApiClient
 import com.example.biophonie.network.ApiError
 import com.example.biophonie.network.ApiInterface
@@ -42,20 +40,8 @@ class BottomSheetFragment : Fragment() {
     private var state: Int = 0
     private var shortAnimationDuration: Int = 0
 
-    private lateinit var parentView: View
+    private lateinit var binding: BottomSheetLayoutBinding
     private lateinit var mListener: SoundSheetListener
-    private lateinit var location: TextView
-    private lateinit var date: TextView
-    private lateinit var coords: TextView
-    private lateinit var close: ImageView
-    private lateinit var waveForm: ImageView
-    private lateinit var image: ImageView
-    private lateinit var left: TextView
-    private lateinit var datePicker: TextView
-    private lateinit var right: TextView
-    private lateinit var expand: TextView
-    private lateinit var pin: ConstraintLayout
-    private lateinit var progressBar: ProgressBar
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
 
     override fun onCreateView(
@@ -63,50 +49,42 @@ class BottomSheetFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        parentView = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
-        bottomSheetBehavior = BottomSheetBehavior.from(parentView)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.bottom_sheet_layout,
+            container,
+            false)
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.root)
 
         /* Trying to make fitsSystemWindow work */
         activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
 
-        location = parentView.findViewById(R.id.location)
-        date = parentView.findViewById(R.id.date)
-        coords = parentView.findViewById(R.id.coordinates)
-
-        close = parentView.findViewById(R.id.close)
-        close.setOnClickListener {
+        binding.close.setOnClickListener {
             if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
             else
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
 
-        image = parentView.findViewById(R.id.sound_image)
-        image.setOnClickListener {
+        binding.soundImage.setOnClickListener {
             /*fitsSystemWindow = true on Fragment not working as expected. Use this empty listener to avoid map dragging while dragging fragment*/
         }
 
-        waveForm = parentView.findViewById(R.id.wave_form)
-        waveForm.setOnClickListener { Toast.makeText(parentView.context, "Lecture du son", Toast.LENGTH_SHORT).show() }
+        binding.waveForm.setOnClickListener { Toast.makeText(activity, "Lecture du son", Toast.LENGTH_SHORT).show() }
 
-        left = parentView.findViewById(R.id.left)
-        left.setOnClickListener {
+        binding.left.setOnClickListener {
             soundsIterator.previous()
             val sound: Sound = soundsIterator.previous()
             displaySound(sound)
         }
 
-        datePicker = parentView.findViewById(R.id.date_picker)
-
-        right = parentView.findViewById(R.id.right)
-        right.setOnClickListener {
+        binding.right.setOnClickListener {
             val sound: Sound = soundsIterator.next()
             displaySound(sound)
         }
 
 
-        expand = parentView.findViewById(R.id.expand)
-        expand.setOnClickListener {
+        binding.expand.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
             if (!imageDisplayed){
                 displayImage()
@@ -115,16 +93,14 @@ class BottomSheetFragment : Fragment() {
                 displayWaveForm()
             }
         }
-        progressBar = parentView.findViewById(R.id.progress_bar)
-        pin = parentView.findViewById(R.id.pin)
-        pin.translationY = 0F
+        binding.pin.translationY = 0F
 
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-        parentView.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 measure()
 
-                val obs: ViewTreeObserver = parentView.viewTreeObserver
+                val obs: ViewTreeObserver = binding.root.viewTreeObserver
                 obs.removeOnGlobalLayoutListener(this)
             }
         })
@@ -133,7 +109,7 @@ class BottomSheetFragment : Fragment() {
             // No other solution was found to pin a view to the bottom of the BottomSheet
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
                     val bottomSheetVisibleHeight = bottomSheet.height - bottomSheet.top
-                    pin.translationY = (bottomSheetVisibleHeight - heightExpanded).toFloat()
+                binding.pin.translationY = (bottomSheetVisibleHeight - heightExpanded).toFloat()
                 }
 
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -148,21 +124,21 @@ class BottomSheetFragment : Fragment() {
                         }
                         BottomSheetBehavior.STATE_HALF_EXPANDED -> {
                             state = BottomSheetBehavior.STATE_HALF_EXPANDED
-                            close.setImageResource(R.drawable.ic_marker)
+                            binding.close.setImageResource(R.drawable.ic_marker)
                             if (imageDisplayed) {
                                 displayWaveForm()
                             }
-                            waveForm.apply{requestLayout()}.layoutParams.height = dpToPx(150)
+                            binding.waveForm.apply{requestLayout()}.layoutParams.height = dpToPx(150)
                             //(bottomSheetBehavior as? LockableBottomSheetBehavior<*>)?.lock()
                         }
                         BottomSheetBehavior.STATE_EXPANDED -> {
                             state = BottomSheetBehavior.STATE_EXPANDED
-                            close.setImageResource(R.drawable.arrow_down)
-                            waveForm.apply{requestLayout()}.layoutParams.height = 0
+                            binding.close.setImageResource(R.drawable.arrow_down)
+                            binding.waveForm.apply{requestLayout()}.layoutParams.height = 0
                             //(bottomSheetBehavior as? LockableBottomSheetBehavior<*>)?.unlock()
                         }
                         else -> {
-                            close.setImageResource(R.drawable.ic_marker)
+                            binding.close.setImageResource(R.drawable.ic_marker)
                             //(bottomSheetBehavior as? LockableBottomSheetBehavior<*>)?.unlock()
                         }
                     }
@@ -170,7 +146,7 @@ class BottomSheetFragment : Fragment() {
             })
 
         shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
-        return parentView
+        return binding.root
     }
 
     private fun crossfade(fadeIn: View, fadeOut: View) {
@@ -266,75 +242,82 @@ class BottomSheetFragment : Fragment() {
 
     private fun displaySound(sound: Sound) {
         checkClickability(geoPoint.sounds!!)
-        location.text = geoPoint.name
-        date.text = SimpleDateFormat("dd/MM/yy", Locale.FRANCE).format(sound.date.time)
-        Log.d(TAG, "displaySound: "+SimpleDateFormat("dd/MM/yy", Locale.FRANCE).format(sound.date.time))
-        coords.text = geoPoint.coordinatesToString()
-        datePicker.text = SimpleDateFormat("MMM yyyy", Locale.FRANCE).format(sound.date.time)
+        binding.apply {
+            location.text = geoPoint.name
+            date.text = SimpleDateFormat("dd/MM/yy", Locale.FRANCE).format(sound.date.time)
+            coordinates.text = geoPoint.coordinatesToString()
+            datePicker.text = SimpleDateFormat("MMM yyyy", Locale.FRANCE).format(sound.date.time)
+        }
         changeWidgetsVisibility(true)
     }
 
     private fun displayImage(){
-        crossfade(image, waveForm)
-        expand.text = "Voir le son"
+        crossfade(binding.soundImage, binding.waveForm)
+        binding.expand.text = "Voir le son"
         imageDisplayed = true
     }
 
     private fun displayWaveForm(){
-        crossfade(waveForm, image)
-        waveForm.layoutParams.height = 0
-        expand.text = "Voir l'image"
+        crossfade(binding.waveForm, binding.soundImage)
+        binding.waveForm.layoutParams.height = 0
+        binding.expand.text = "Voir l'image"
         imageDisplayed = false
     }
 
     private fun measure() {
-        bottomSheetBehavior.peekHeight = pin.height*2
-        val screenHeight: Int = DisplayMetrics().also { activity!!.windowManager.defaultDisplay.getMetrics(it) }.heightPixels
-        heightExpanded = 2*parentView.height - parentView.top // A bit mysterious but it works
-        bottomSheetBehavior.halfExpandedRatio = (pin.height*2 + waveForm.height).toFloat() / screenHeight.toFloat()
+        binding.apply{
+            bottomSheetBehavior.peekHeight = pin.height*2
+            val screenHeight: Int = DisplayMetrics().also { activity!!.windowManager.defaultDisplay.getMetrics(it) }.heightPixels
+            heightExpanded = 2*container.height - container.top // A bit mysterious but it works
+            bottomSheetBehavior.halfExpandedRatio = (pin.height*2 + waveForm.height).toFloat() / screenHeight.toFloat()
+        }
     }
 
     private fun checkClickability(sounds: List<Sound>){
         // A bit of a hack due to ListIterators' behavior.
         // The index is between two elements.
-        try {
-            Log.d(TAG, "checkClickability: previous URL " + sounds[soundsIterator.previousIndex()-1].urlAudio)
-            left.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
-            left.isClickable = true
-        } catch (e: IndexOutOfBoundsException){
-            left.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
-            left.isClickable = false
-        }
-        try{
-            Log.d(TAG, "checkClickability: next URL "+ sounds[soundsIterator.nextIndex()].urlAudio)
-            right.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
-            right.isClickable = true
-        } catch (e: IndexOutOfBoundsException){
-            right.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
-            right.isClickable = false
+        binding.apply {
+            try {
+                Log.d(TAG, "checkClickability: previous URL " + sounds[soundsIterator.previousIndex()-1].urlAudio)
+                left.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+                left.isClickable = true
+            } catch (e: IndexOutOfBoundsException){
+                left.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
+                left.isClickable = false
+            }
+            try{
+                Log.d(TAG, "checkClickability: next URL "+ sounds[soundsIterator.nextIndex()].urlAudio)
+                right.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+                right.isClickable = true
+            } catch (e: IndexOutOfBoundsException){
+                right.setTextColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
+                right.isClickable = false
+            }
         }
     }
 
     private fun changeWidgetsVisibility(makeVisible: Boolean){
-        if (makeVisible){
-            location.visibility = View.VISIBLE
-            date.visibility = View.VISIBLE
-            coords.visibility = View.VISIBLE
-            close.visibility = View.VISIBLE
-            waveForm.visibility = View.VISIBLE
-            pin.visibility = View.VISIBLE
+        binding.apply {
+            if (makeVisible){
+                location.visibility = View.VISIBLE
+                date.visibility = View.VISIBLE
+                coordinates.visibility = View.VISIBLE
+                close.visibility = View.VISIBLE
+                waveForm.visibility = View.VISIBLE
+                pin.visibility = View.VISIBLE
 
-            progressBar.visibility = View.GONE
-        }
-        else{
-            location.visibility = View.GONE
-            date.visibility = View.GONE
-            coords.visibility = View.GONE
-            close.visibility = View.GONE
-            waveForm.visibility = View.GONE
-            pin.visibility = View.GONE
+                progressBar.visibility = View.GONE
+            }
+            else{
+                location.visibility = View.GONE
+                date.visibility = View.GONE
+                coordinates.visibility = View.GONE
+                close.visibility = View.GONE
+                waveForm.visibility = View.GONE
+                pin.visibility = View.GONE
 
-            progressBar.visibility = View.VISIBLE
+                progressBar.visibility = View.VISIBLE
+            }
         }
     }
 
