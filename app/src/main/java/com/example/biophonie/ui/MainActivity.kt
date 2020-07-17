346f3f3ec3f4cae2f87304fab3155e15e559b140
Haran <ara.rahier@gmail.com>
2020-07-17 11:00:09 +0200

    :construction: Start a tutorial on first time use

--- app/src/main/java/com/example/biophonie/ui/MainActivity.kt
+++ app/src/main/java/com/example/biophonie/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.biophonie.ui
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.biophonie.R
 
+
 class MainActivity : AppCompatActivity() {
+    private val myPrefs = "Preferences"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         buttonToMap.setOnClickListener {
             startActivity(Intent(this, MapActivity::class.java))
         }
+        checkTutorial()
+    }
+
+    private fun checkTutorial() {
+        val sp = getSharedPreferences(myPrefs, Context.MODE_PRIVATE)
+        //if (!sp.getBoolean("first", false)) {
+            val editor = sp.edit()
+            editor.putBoolean("first", true)
+            editor.apply()
+            Toast.makeText(this, "First", Toast.LENGTH_SHORT).show()
+            /*val intent =
+                Intent(this, SampleCirclesDefault::class.java) //call your ViewPager class*/
+            startActivity(intent)
+        //}
     }
 }
