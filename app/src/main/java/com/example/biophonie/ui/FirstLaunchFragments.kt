ea8b29962c1094a9f22d57e43cef01c9948e4dea
Haran <ara.rahier@gmail.com>
2020-07-21 11:20:42 +0200

    :sparkles: Request name inside tutorial
    Stored inside sharedprefs. Not the definitive UI. This one is juste a
    prototype, and it's not a convenient one.

---
+++ app/src/main/java/com/example/biophonie/ui/FirstLaunchFragments.kt
@@ -0,0 +1,33 @@
+package com.example.biophonie.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.example.biophonie.R
+
+class TutorialFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_tutorial, container, false)
+}
+
+private const val TAG = "FirstLaunchFragments"
+class NameFragment: Fragment(){
+    lateinit var name: EditText
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_name, container, false)
+        name = view.findViewById<EditText>(R.id.name)
+        return view
+    }
+}
--- app/src/main/java/com/example/biophonie/ui/TutorialActivity.kt
+++ app/src/main/java/com/example/biophonie/ui/TutorialActivity.kt
@@ -1,10 +1,11 @@
 package com.example.biophonie.ui
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -17,9 +18,11 @@ import com.google.android.material.tabs.TabLayoutMediator
 private const val NUM_PAGES = 3
 private const val TAG = "TutorialActivity"
 
+//TODO maybe get permissions inside tutorial ?
 class TutorialActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityTutorialBinding
+    private val adapter = TutorialPagerAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +34,7 @@ class TutorialActivity : FragmentActivity() {
 
     private fun setUpListeners() {
         binding.apply {
-            skip.setOnClickListener { startMapActivity() }
+            skip.setOnClickListener { pager.currentItem = NUM_PAGES -1 }
             pager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
 
                 override fun onPageSelected(position: Int) {
@@ -54,14 +57,25 @@ class TutorialActivity : FragmentActivity() {
     }
 
     private fun startMapActivity() {
-        startActivity(
-            Intent(this, MapActivity::class.java)
-                .apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK) })
+        if(adapter.nameFragment.name.text.toString().isNotEmpty()){
+            storeName(adapter.nameFragment.name.text.toString())
+            startActivity(
+                Intent(this, MapActivity::class.java)
+                    .apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK) })
+        }
+        else
+            Toast.makeText(this, "Veuillez donner un nom", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun storeName(name: String){
+        val prefs = getSharedPreferences(null, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("name", name)
+        editor.apply()
     }
 
     private fun setUpViewPager() {
-        val pagerAdapter = TutorialPagerAdapter(this)
-        binding.pager.adapter = pagerAdapter
+        binding.pager.adapter = adapter
 
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
             //tab.view.isClickable = false
@@ -78,9 +92,16 @@ class TutorialActivity : FragmentActivity() {
 
 
     private inner class TutorialPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+
+        val nameFragment = NameFragment()
         override fun getItemCount(): Int = NUM_PAGES
 
-        override fun createFragment(position: Int): Fragment = TutorialFragment()
+        override fun createFragment(position: Int): Fragment{
+            return when(position){
+                NUM_PAGES-1 -> nameFragment
+                else -> TutorialFragment()
+            }
+        }
     }
 
 }
