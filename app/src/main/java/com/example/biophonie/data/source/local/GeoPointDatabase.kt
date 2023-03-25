fc7b78a6353de68cfeb35390c2dde10ec5988479
Alban Rahier <arahier@greenspector.com>
2023-03-25 23:07:04 +0100

    :zap: export database schema in prod

--- app/src/main/java/com/example/biophonie/data/source/local/GeoPointDatabase.kt
+++ app/src/main/java/com/example/biophonie/data/source/local/GeoPointDatabase.kt
@@ -3,10 +3,11 @@ package com.example.biophonie.data.source.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.biophonie.BuildConfig
 import com.example.biophonie.data.source.Converters
 import com.example.biophonie.data.source.DatabaseGeoPoint
 
-@Database(entities = [DatabaseGeoPoint::class], version = 6, exportSchema = false /* true in prod */)
+@Database(entities = [DatabaseGeoPoint::class], version = 6, exportSchema = BuildConfig.BUILD_TYPE != "debug")
 @TypeConverters(Converters::class)
 abstract class GeoPointDatabase : RoomDatabase() {
 
