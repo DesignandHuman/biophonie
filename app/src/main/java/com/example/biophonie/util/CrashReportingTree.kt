d28cccb0d8294311f82027a70cb1101cc172fd46
Alban Rahier <arahier@greenspector.com>
2023-03-26 06:51:46 +0200

    :bug: fix crash when logging error

--- app/src/main/java/com/example/biophonie/util/CrashReportingTree.kt
+++ app/src/main/java/com/example/biophonie/util/CrashReportingTree.kt
@@ -1,6 +1,7 @@
 package com.example.biophonie.util
 
 import android.util.Log
+import com.example.biophonie.BuildConfig
 import timber.log.Timber
 
 class ReleaseTree: Timber.DebugTree() {
@@ -8,6 +9,6 @@ class ReleaseTree: Timber.DebugTree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         // Crashlytics ?
         if (priority == Log.ERROR)
-            e(t, message, tag)
+            Log.wtf(BuildConfig.APPLICATION_ID, message, t)
     }
 }
