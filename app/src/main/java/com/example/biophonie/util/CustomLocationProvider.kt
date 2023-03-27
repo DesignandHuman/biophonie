212cf13cad2ee7de4b0cd0326d2e30dc41f87eb9
Alban Rahier <arahier@greenspector.com>
2023-03-27 18:12:14 +0200

    :bug: fix location provider not called properly

--- app/src/main/java/com/example/biophonie/util/CustomLocationProvider.kt
+++ app/src/main/java/com/example/biophonie/util/CustomLocationProvider.kt
@@ -4,126 +4,146 @@ import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
-import com.mapbox.common.location.compat.*
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting.PRIVATE
+import com.mapbox.android.core.location.LocationEngineCallback
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.location.LocationEngineRequest
+import com.mapbox.android.core.location.LocationEngineResult
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.geojson.Point
-import com.mapbox.maps.plugin.locationcomponent.BuildConfig
+import com.mapbox.maps.logE
+import com.mapbox.maps.logW
+import com.mapbox.maps.plugin.PuckBearingSource
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentConstants
 import com.mapbox.maps.plugin.locationcomponent.LocationConsumer
+import com.mapbox.maps.plugin.locationcomponent.LocationConsumer2
 import com.mapbox.maps.plugin.locationcomponent.LocationProvider
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import timber.log.Timber
 import java.lang.ref.WeakReference
+import java.util.concurrent.CopyOnWriteArraySet
+import kotlin.coroutines.coroutineContext
 
+/**
+ * Custom Location Provider implementation adapted from mapbox DefaultLocationProvider
+ */
 class CustomLocationProvider(
-    context: Context,
-    locationUpdatesInterval: Long = LOCATION_UPDATES_INTERVAL,
-    locationUpdatesMaxWaitTime: Long = LOCATION_UPDATES_MAX_WAIT_TIME,
-    private val locationEngine: LocationEngine = LocationEngineProvider.getBestLocationEngine(context),
+    context: Context
 ) : LocationProvider {
 
-    companion object {
-        private const val LOCATION_UPDATES_INTERVAL = 200L
+    private val applicationContext = context.applicationContext
+    private val locationEngine = LocationEngineProvider.getBestLocationEngine(applicationContext)
+    private val locationEngineRequest =
+        LocationEngineRequest.Builder(LocationComponentConstants.DEFAULT_INTERVAL_MILLIS)
+            .setFastestInterval(LocationComponentConstants.DEFAULT_FASTEST_INTERVAL_MILLIS)
+            .setPriority(LocationEngineRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+            .build()
 
-        private const val LOCATION_UPDATES_MIN_WAIT_TIME = 50L
-        private const val LOCATION_UPDATES_MAX_WAIT_TIME = LOCATION_UPDATES_INTERVAL * 5
+    private val locationConsumers = CopyOnWriteArraySet<LocationConsumer>()
+    private var updateDelay = INIT_UPDATE_DELAY
+    private val job = CoroutineScope(Dispatchers.IO).launch (start = CoroutineStart.LAZY) {
+        delay(updateDelay)
+        requestLocationUpdates()
     }
 
-    class LocationUpdatedCallback(provider: CustomLocationProvider) :
+    private val locationEngineCallback: LocationEngineCallback<LocationEngineResult> =
+        CurrentLocationEngineCallback(this)
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationUpdates() {
+        if (PermissionsManager.areLocationPermissionsGranted(applicationContext)) {
+            locationEngine.requestLocationUpdates(
+                locationEngineRequest, locationEngineCallback, Looper.getMainLooper()
+            )
+        } else {
+            if (updateDelay * 2 < MAX_UPDATE_DELAY) {
+                updateDelay *= 2
+            } else {
+                updateDelay = MAX_UPDATE_DELAY
+            }
+            job.start()
+            Timber.w("Missing location permission, location component will not take effect before location permission is granted.")
+        }
+    }
+
+    private fun notifyLocationUpdates(location: Location) {
+        locationConsumers.forEach { consumer ->
+            consumer.onLocationUpdated(Point.fromLngLat(location.longitude, location.latitude))
+            consumer.onBearingUpdated(location.bearing.toDouble())
+            if (consumer is LocationConsumer2) {
+                consumer.onAccuracyRadiusUpdated(location.accuracy.toDouble())
+            }
+        }
+    }
+
+    fun addSingleRequestLocationConsumer(callback: (Point.() -> Unit)) {
+        this.registerLocationConsumer(object : LocationConsumer {
+            override fun onBearingUpdated(
+                vararg bearing: Double,
+                options: (ValueAnimator.() -> Unit)?
+            ) {
+            }
+
+            override fun onLocationUpdated(
+                vararg location: Point,
+                options: (ValueAnimator.() -> Unit)?
+            ) {
+                callback(location[0])
+                this@CustomLocationProvider.unRegisterLocationConsumer(this)
+            }
+
+            override fun onPuckBearingAnimatorDefaultOptionsUpdated(options: ValueAnimator.() -> Unit) {
+            }
+
+            override fun onPuckLocationAnimatorDefaultOptionsUpdated(options: ValueAnimator.() -> Unit) {
+            }
+        })
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun registerLocationConsumer(locationConsumer: LocationConsumer) {
+        if (locationConsumers.isEmpty()) {
+            requestLocationUpdates()
+        }
+        locationConsumers.add(locationConsumer)
+        if (PermissionsManager.areLocationPermissionsGranted(applicationContext)) {
+            locationEngine.getLastLocation(locationEngineCallback)
+        } else {
+            Timber.w("Missing location permission, location component will not take effect before location permission is granted.")
+        }
+    }
+
+    override fun unRegisterLocationConsumer(locationConsumer: LocationConsumer) {
+        locationConsumers.remove(locationConsumer)
+        if (locationConsumers.isEmpty()) {
+            locationEngine.removeLocationUpdates(locationEngineCallback)
+            GlobalScope.launch { job.cancel() }
+        }
+    }
+
+    // Callbacks may leak after GoogleLocationEngineImpl.removeLocationUpdates,
+    // see https://github.com/mapbox/mapbox-events-android/issues/562 for more details
+    private class CurrentLocationEngineCallback(locationProvider: CustomLocationProvider) :
         LocationEngineCallback<LocationEngineResult> {
+        private val locationProviderWeakReference: WeakReference<CustomLocationProvider> =
+            WeakReference(locationProvider)
 
-        // weak reference to avoid memory leak
-        private val provider = WeakReference(provider)
-
-        override fun onSuccess(result: LocationEngineResult?) {
-            result?.lastLocation?.let {
-                provider.get()?.updateLocation(Point.fromLngLat(it.longitude, it.latitude, it.altitude))
-                provider.get()?.updateBearing(it.bearing.toDouble())
+        override fun onSuccess(result: LocationEngineResult) {
+            result.lastLocation?.let { location ->
+                locationProviderWeakReference.get()?.notifyLocationUpdates(location)
             }
         }
 
         override fun onFailure(exception: Exception) {
-            Timber.e("onFailure: ${exception.localizedMessage}")
+            Timber.e("Failed to obtain location update: $exception")
         }
     }
 
-    private val consumers: HashSet<LocationConsumer> = hashSetOf()
-    private val onLocationUpdated = LocationUpdatedCallback(this)
-
-    init {
-        val request = LocationEngineRequest.Builder(locationUpdatesInterval)
-            .setFastestInterval(LOCATION_UPDATES_MIN_WAIT_TIME)
-            .setMaxWaitTime(locationUpdatesMaxWaitTime)
-            .setDisplacement(0f)
-            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-            .build()
-
-        try {
-            locationEngine.apply {
-                requestLocationUpdates(request, onLocationUpdated, context.mainLooper)
-            }
-        } catch (e: SecurityException) {
-            Timber.e(e)
-        }
-    }
-
-    private fun updateLocation(point: Point) {
-        Timber.i("updateLocation: $point")
-        consumers.forEach { it.onLocationUpdated(point) }
-    }
-
-    private fun updateBearing(bearing: Double) {
-        Timber.i("updateBearing: $bearing")
-        consumers.forEach { it.onBearingUpdated(bearing) }
-    }
-
-    @SuppressLint("MissingPermission")
-    fun addSingleRequestLocationConsumer(callback: (Point.() -> Unit)) {
-        if (com.example.biophonie.BuildConfig.BUILD_TYPE != "debug") {
-            this.registerLocationConsumer(object : LocationConsumer {
-                override fun onBearingUpdated(
-                    vararg bearing: Double,
-                    options: (ValueAnimator.() -> Unit)?
-                ) {
-                }
-
-                override fun onLocationUpdated(
-                    vararg location: Point,
-                    options: (ValueAnimator.() -> Unit)?
-                ) {
-                    callback(location[0])
-                    this@CustomLocationProvider.unRegisterLocationConsumer(this)
-                }
-
-                override fun onPuckBearingAnimatorDefaultOptionsUpdated(options: ValueAnimator.() -> Unit) {
-                }
-
-                override fun onPuckLocationAnimatorDefaultOptionsUpdated(options: ValueAnimator.() -> Unit) {
-                }
-            })
-        }
-        else { //this method can give outdated location so used only in debug
-            locationEngine.getLastLocation(object: LocationEngineCallback<LocationEngineResult> {
-                override fun onFailure(exception: Exception) {
-                    Timber.e(exception)
-                }
-
-                override fun onSuccess(result: LocationEngineResult?) {
-                    result?.lastLocation?.let {
-                        Timber.d("${it.longitude},${it.latitude}")
-                        callback(Point.fromLngLat(it.longitude,it.latitude))
-                    }
-                }
-            })
-        }
-    }
-
-    override fun registerLocationConsumer(locationConsumer: LocationConsumer) {
-        consumers.add(locationConsumer)
-    }
-
-    override fun unRegisterLocationConsumer(locationConsumer: LocationConsumer) {
-        consumers.remove(locationConsumer)
+    private companion object {
+        private const val INIT_UPDATE_DELAY = 100L
+        private const val MAX_UPDATE_DELAY = 5000L
     }
 }
