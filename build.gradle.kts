950ba44029803d3959e9adde0a22b41eef565fa8
Alban Rahier <arahier@greenspector.com>
2024-01-16 15:32:44 +0100

    :recycle: move repository definition into settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,6 @@ plugins {
     alias(libs.plugins.ksp) apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-        maven {
-            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-            credentials {
-                username = "mapbox"
-                password = (project.properties["MAPBOX_DOWNLOAD_TOKEN"] ?: "") as String
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean"){
     delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,27 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = (extra["MAPBOX_DOWNLOAD_TOKEN"] ?: "") as String
+            }
+        }
+    }
+}
+
 // integrate local soundwave dependency
 // requires soundwave PATH to be defined in global gradle.properties extra SOUNDWAVE_DIR
 // TODO: consider moving WaveFormPlayer into this project
